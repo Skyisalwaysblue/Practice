32057a16c45125477a6a0b0223f705842077e887
Skyisalwaysblue <ghmahmoodi5@gmail.com>
2023-02-23 18:41:52 -0500

    first commit

--- src/newgitpractice/GitPracticeone.java
+++ src/newgitpractice/GitPracticeone.java
@@ -4,7 +4,8 @@ public class GitPracticeone {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This is for push practice");
+		//System.out.println("This is for push practice");
+		System.out.println("This is practice for pull");
 
 	}
 
