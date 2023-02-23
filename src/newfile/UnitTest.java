3938c5d598ae785c92b2f136e4df85927d986106
Skyisalwaysblue <ghmahmoodi5@gmail.com>
2023-02-23 15:47:06 -0500

    new method added and old one commented

--- src/newfile/UnitTest.java
+++ src/newfile/UnitTest.java
@@ -3,11 +3,18 @@ package newfile;
 public class UnitTest {
 	public static void main(String[]args) {
 		
-		int a = 10;
-		int b = 20;
-		int c = a+b;
-		System.out.println(c);
-				
+		//int a = 10;
+		//int b = 20;
+		//int c = a+b;
+		
+		//System.out.println(c);
+		
+		System.out.println(testOne(10,5));
+	}
 
-}
+
+	
+	public static int testOne(int a, int b) {
+		return a - b;
+	}
 }
