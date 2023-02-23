226f01558ab86325c06feb781a39780a617add92
Skyisalwaysblue <ghmahmoodi5@gmail.com>
2023-02-23 18:46:57 -0500

    new commit

--- src/newgitpractice/GitPracticeone.java
+++ src/newgitpractice/GitPracticeone.java
@@ -7,6 +7,7 @@ public class GitPracticeone {
 		//System.out.println("This is for push practice");
 		System.out.println("This is practice for pull");
 
+		System.out.println("new changes");
 	}
 
 }
